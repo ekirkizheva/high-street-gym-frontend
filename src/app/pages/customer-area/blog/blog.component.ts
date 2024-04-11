28cc7d9d7a8da2392d1b6ac547894f8b38eca854
ekirkizheva <kirkizhevaelena@gmail.com>
2024-04-12 00:41:29 +1000

    add scroll hack

--- src/app/pages/customer-area/blog/blog.component.ts
+++ src/app/pages/customer-area/blog/blog.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { BehaviorSubject, Observable, switchMap, take } from 'rxjs';
+import { BehaviorSubject, Observable, startWith, switchMap, take } from 'rxjs';
 import { Blog, BlogService } from './blog.service';
 
 @Component({
@@ -21,15 +21,20 @@ export class BlogComponent {
   constructor(
     private fb: FormBuilder,
     private blogService: BlogService) {
-    this.blog$ = this.refreshSubject$.pipe(switchMap(() => this.blogService.blog$));
+    this.blog$ = this.refreshSubject$.pipe(
+      switchMap(() => this.blogService.blog$.pipe(
+        startWith([]) // hack to force page to scroll to the top
+      )));
   }
 
   onSubmit() {
     if(this.blogForm.valid) {
-      this.blogService.postBlog$(this.blogForm.get('message')?.value!).pipe(take(1)).subscribe(() => {
-        this.refreshSubject$.next(true);
-        this.blogForm.reset();
-      });
+      this.blogService.postBlog$(this.blogForm.get('message')?.value!)
+        .pipe(take(1))
+        .subscribe(() => {
+          this.refreshSubject$.next(true);
+          this.blogForm.reset();
+        });
     }
   }
 }
