d13d09e1f40ce669d597d42b402ec99e300752f4
ekirkizheva <kirkizhevaelena@gmail.com>
2024-04-13 09:41:16 +1000

    remove old pages

--- src/app/pages/admin-area/admin-area.module.ts
+++ src/app/pages/admin-area/admin-area.module.ts
@@ -3,10 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AdminAreaRoutingModule } from './admin-area.routing.module';
-import { ChangeLogComponent } from './change-log/change-log.component';
 import { HomeComponent } from './home/home.component';
-import { ManageProductsModifyComponent } from './manage-products-modify/manage-products-modify.component';
-import { ManageProductsComponent } from './manage-products/manage-products.component';
 import { ManageUsersModifyComponent } from './manage-users-modify/manage-users-modify.component';
 import { ManageUsersComponent } from './manage-users/manage-users.component';
 import { XmlUploadComponent } from './xml-upload/xml-upload.component';
@@ -14,11 +11,8 @@ import { XmlUploadComponent } from './xml-upload/xml-upload.component';
 @NgModule({
   declarations: [
     HomeComponent,
-    ChangeLogComponent,
     ManageUsersComponent,
-    ManageProductsComponent,
     ManageUsersModifyComponent,
-    ManageProductsModifyComponent,
     XmlUploadComponent
   ],
   imports: [
